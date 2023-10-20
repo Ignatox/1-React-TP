b7c064ac30090dcf8a3b555cf3ea85123340b471
Ignatox <ignaciodiaz93@gmail.com>
2023-10-20 19:13:43 -0300

    FrontEndDesigns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import Header from "./components/Header/Header" 
+
+import CarouselHome from "./components/CarouselHome/CarouselHome"
+import Header from "./components/Header/Header"
+import AboutUs from "./components/AboutUs/AboutUs"
 function App() {
   return (
     <>
-      </Header>
+      <Header/>
+      <CarouselHome/>
+      <p>About Us</p>
+      Know more about us clicking on the text fields.
+      <AboutUs/>
     </>
   )
 }
