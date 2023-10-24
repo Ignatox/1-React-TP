03f6e384be21db35e8aa92c95120f18b5a1212d5
Ignatox <ignaciodiaz93@gmail.com>
2023-10-24 12:34:39 -0300

    Metodos http para consultas a la api

---
+++ src/services/ProductService.ts
@@ -0,0 +1,51 @@
+//Metodos para hacer el ABM completo
+
+import {Product} from '../types/Products';
+
+const BASE_URL = 'https://fakestoreapi.com';
+
+export const ProductService = {
+    //Aca declaramos los metodos
+    getProducts: async () : Promise<Product[]>=> {
+        const response = await fetch(`${BASE_URL}/products`);
+        const data = await response.json();
+        return data;
+    },
+    getProduct:async(id:number) : Promise <Product> => {
+        const response = await fetch(`${BASE_URL}/products/${id}`);
+        const data = await response.json();
+        return data;
+    },
+
+    createProduct: async(product:Product): Promise<Product> => {
+        const response = await fetch(`${BASE_URL}/products`, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        });
+        const data =await response.json();
+        return data;
+
+    },
+
+    updateProduct: async(id:number, product:Product) : Promise<Product> => {
+        const response= await fetch(`${BASE_URL}/products/${id}`,{
+            method: "PUT",
+            headers : {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+    });
+    const data =await response.json();
+    return data;
+    },
+    deleteProduct: async(id:number): Promise<void> => {
+        await fetch (
+            `${BASE_URL}/products/${id}`,{
+                method: "DELETE"
+            });
+        
+    }
+}
